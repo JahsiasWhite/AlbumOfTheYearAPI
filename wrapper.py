07a309d519a9dcd2a561b93cca685484db475d2e
Jah <jahsias.white@gmail.com>
2022-03-04 23:28:02 -0600

    changed functions to output JSON

--- wrapper.py
+++ wrapper.py
@@ -2,39 +2,52 @@
 #import requests
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
+import json
 
 
 class User(object):
 
-    def __init__(self, user):
+    def getUserPage(self, user):
         req = Request('https://www.albumoftheyear.org/user/{}'.format(user), headers={'User-Agent': 'Mozilla/6.0'})
         page = urlopen(req).read()
         soup = BeautifulSoup(page, 'html.parser')
         return soup
 
-    #def getUser(self, user):
-        
-
     def getUserRatings(self, user, soup):
         obj = soup.find(href='/user/{}'.format(user)+'/ratings/')
         ratings =  obj.find(class_='profileStat').getText()
-        return ratings
+        ratings_JSON = {
+            "ratings" : ratings
+        }
+        return json.dumps(ratings_JSON)
     
     def getUserReviews(self, user, soup):
         obj = soup.find(href='/user/{}'.format(user)+'/reviews/')
         reviews =  obj.find(class_='profileStat').getText()
-        return reviews
+        reviews_JSON = {
+            "reviews" : reviews
+        }
+        return json.dumps(reviews_JSON)
 
     def getUserLists(self, user, soup):
         obj = soup.find(href='/user/{}'.format(user)+'/lists/')
-        Lists =  obj.find(class_='profileStat').getText()
-        return Lists
+        lists =  obj.find(class_='profileStat').getText()
+        lists_JSON = {
+            "lists" : lists
+        }
+        return json.dumps(lists_JSON)
 
     def getUserFollowers(self, user, soup):
         obj = soup.find(href='/user/{}'.format(user)+'/followers/')
-        Followers =  obj.find(class_='profileStat').getText()
-        return Followers
+        followers =  obj.find(class_='profileStat').getText()
+        followers_JSON = {
+            "followers" : followers
+        }
+        return json.dumps(followers_JSON)
 
     def getUserAbout(self, user, soup):
-        aboutUser =  soup.find(class_='aboutUser').getText()
-        return aboutUser
+        about_user =  soup.find(class_='aboutUser').getText()
+        about_user_JSON = {
+            "about_user" : about_user
+        }
+        return json.dumps(about_user_JSON)
