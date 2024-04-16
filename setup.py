245cd1fa149d8cb8837ace3c3efb07a378d6c13a
Jah <jahsias.white@gmail.com>
2024-04-15 23:13:25 -0500

    Potential fix for import failure

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from setuptools import setup, find_packages
+# from setuptools import setup, find_packages
+import setuptools
 
 with open("requirements.txt", encoding="utf-8") as f:
     install_requires = f.read().splitlines()
@@ -10,14 +11,14 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 
-setup(
+setuptools.setup(
     name="album-of-the-year-api",
     description="A light weight Python library that acts as an API for the website albumoftheyear.org",
     version="0.2.5",
     license="GNU",
     author="Jahsias White",
     author_email="jahsias.white@gmail.com",
-    find_packages=["albumoftheyearapi"],
+    packages=["albumoftheyearapi"],
     install_requires=install_requires,
     long_description=long_description,
     long_description_content_type="text/markdown",
